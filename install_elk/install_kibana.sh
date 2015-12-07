122ce546233097343834ddad1c6f8861d3cd8c72
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-07 17:44:50 +0900

    update

--- install_elk/install_kibana.sh
+++ install_elk/install_kibana.sh
@@ -4,8 +4,8 @@ echo 0 >/selinux/enforce
 groupadd kibana
 useradd -g kibana  kibana
 wget -N -P /opt/ https://download.elastic.co/kibana/kibana/kibana-4.3.0-linux-x64.tar.gz
-tar xvf /opt/kibana-*.tar.gz
-mv /opt/kibana* /opt/kibana
+cd /opt;tar xvf kibana-4.3.0-linux-x64.tar.gz
+mv kibana-4.3.0-linux-x64 kibana
 yes | cp -rf /userdata/install_elk/kibana.yml /opt/kibana/config/
 chown -R kibana:kibana /opt/kibana
 cd /etc/init.d && sudo curl -o kibana https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/fc5025c3fc499ad8262aff34ba7fde8c87ead7c0/kibana-4.x-init
