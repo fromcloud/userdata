8abbf02aaba42911626a6874b4604d64b93018b2
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-07 17:49:44 +0900

    update 2

--- install_elk/install_logstash.sh
+++ install_elk/install_logstash.sh
@@ -3,7 +3,7 @@
 yes | cp -rp /userdata/install_elk/logstash.repo /etc/yum.repos.d/
 yum -y install /userdata/install_elk/logstash
 yes | cp -rp /userdata/install_elk/openssl.cnf /etc/pki/tls/
-vrip=$(/var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identifier | tail -1 | awk '{print $3}' | tr -d ';')
+vrip=$(cat /var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identifier | tail -1 | awk '{print $3}' | tr -d ';')
 myip=$(curl http://$vrip/latest/local-ipv4)
 sed -i 's/server_ip/$myip/g' /etc/pki/tls/openssl.cnf
 cd /etc/pki/tls;openssl req -config /etc/pki/tls/openssl.cnf -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
