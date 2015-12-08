450fefac81a6bda9686e9fb4d0cbca2b928d18c8
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-08 13:16:05 +0900

    update script

--- install_elk/install_logstash-forwarder.sh
+++ install_elk/install_logstash-forwarder.sh
@@ -4,7 +4,7 @@ rpm --import http://packages.elastic.co/GPG-KEY-elasticsearch
 yes | cp -rf /userdata/install_elk/logstash-forwarder.repo /etc/yum.repos.d/
 yum -y install logstash-forwarder
 yes | cp -rf /userdata/install_elk/logstash-forwarder.conf /etc/
-vrip=$(/var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identifier | tail -1 | awk '{print $3}' | tr -d ';')
+vrip=$(curl /var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identifier | tail -1 | awk '{print $3}' | tr -d ';')
 myip=$(curl http://$vrip/latest/local-ipv4)
 sed -i 's/server_ip/$myip/g' /etc/logstash-forwarder.conf
 service logstash-forwarder start
