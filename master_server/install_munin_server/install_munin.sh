d88084dcecdb99536f93b33d4fc0674a2b2b31ad
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 10:03:29 +0900

    modified_munin

--- master_server/install_munin_server/install_munin.sh
+++ master_server/install_munin_server/install_munin.sh
@@ -3,6 +3,7 @@
 file_path=/userdata/master_server/install_munin_server
 yum upgrade -y ca-certificates
 yum install -y epel-release
+yum install -y nginx httpd-tools
 yum install -y munin munin-node rrdtool
 htpasswd -b -c /etc/nginx/munin-htpasswd.users muninadmin muninadmin123
 yes | cp -rp $file_path/munin.conf /etc/nginx/conf.d/
