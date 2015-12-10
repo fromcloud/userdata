d2f04d67f985e2fdbbe84f52b0a3e08e013cc890
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 17:48:16 +0900

    change munin

--- master_server/install_elk_server/install_kibana.sh
+++ master_server/install_elk_server/install_kibana.sh
@@ -3,7 +3,7 @@
 file_path=/userdata/master_server/install_elk_server
 echo 0 >/selinux/enforce
 groupadd kibana
-useradd -g kibana  kibana
+useradd -g kibana kibana
 wget -N -P /opt/ https://download.elastic.co/kibana/kibana/kibana-4.3.0-linux-x64.tar.gz
 cd /opt;tar xvf kibana-4.3.0-linux-x64.tar.gz
 mv kibana-4.3.0-linux-x64 kibana
