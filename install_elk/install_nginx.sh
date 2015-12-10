744f4c49563a6b76ccbb6249651c3b869dd797f4
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 09:45:03 +0900

    deleted old code

--- install_elk/install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install -y nginx httpd-tools
-htpasswd -b -c /etc/nginx/htpasswd.users kibanaadmin kbnadmin123
-yes | cp -rp /userdata/install_elk/nginx.conf /etc/nginx/
-yes | cp -rp /userdata/install_elk/kibana.conf /etc/nginx/conf.d/
-service nginx start
-chkconfig nginx on
