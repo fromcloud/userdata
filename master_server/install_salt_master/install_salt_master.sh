bc5bcdb638a56e49b0e2b98e81e98986c0edad9b
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 13:41:05 +0900

    change salt

---
+++ master_server/install_salt_master/install_salt_master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file_path=/userdata/master_server/install_salt_master
+yum install -y salt salt-api salt-cloud salt-master salt-minion
+yes | cp -rp $file_path/master /etc/salt/
+yes | mkdir -rp /salt/states/base /salt/states/dev /salt/pillars/base
+chkconfig salt-master on
+service salt-master restart
+
+yes | cp -rp $file_path/minion /etc/salt/
+vrip=$(cat /var/lib/dhclient/dhclient-eth0.leases | grep dhcp-server-identifier | tail -1 | awk '{print $3}' | tr -d ';')
+myip=$(curl http://$vrip/latest/local-ipv4)
+sed -i 's/server_ip/'"$myip"'/g' /etc/salt/minion
+chkconfig salt-minion on
+service salt-minion restart
