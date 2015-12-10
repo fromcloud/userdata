0dedaa78253201db4b8b3f2c05f164a34fc5f80a
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 16:31:36 +0900

    chage htpasswd

--- master_server/install_elk_server/install_nginx.sh
+++ master_server/install_elk_server/install_nginx.sh
@@ -2,7 +2,6 @@
 
 file_path=/userdata/master_server/install_elk_server
 yum install -y nginx httpd-tools
-htpasswd -b -c /etc/nginx/htpasswd.users kibanaadmin kibanadmin123
 yes | cp -rp $file_path/nginx.conf /etc/nginx/
 yes | cp -rp $file_path/kibana.conf /etc/nginx/conf.d/
 service nginx start
