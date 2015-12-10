f3879e26e18c1e714705b615bd2d602b7c147dbb
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 10:58:15 +0900

    change munin

--- master_server/install_munin_server/install_munin.sh
+++ master_server/install_munin_server/install_munin.sh
@@ -6,6 +6,7 @@ yum install -y epel-release
 yum install -y nginx httpd-tools
 yum install -y munin munin-node rrdtool
 htpasswd -b -c /etc/nginx/munin-htpasswd.users muninadmin muninadmin123
+yes | rm -rf /etc/nginx/conf.d/default.conf
 yes | cp -rp $file_path/munin.conf /etc/nginx/conf.d/
 service nginx restart
 chkconfig nginx on
--- master_server/install_munin_server/install_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-file_path=/userdata/install_elk
-yum install -y nginx httpd-tools
-htpasswd -b -c /etc/nginx/kibana-htpasswd.users kibanaadmin kbnadmin123
-yes | cp -rp $file_path/nginx.conf /etc/nginx/
-yes | cp -rp $file_path/kibana.conf /etc/nginx/conf.d/
-service nginx start
-chkconfig nginx on
