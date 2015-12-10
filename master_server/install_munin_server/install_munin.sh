c020ee22bf39e508fedeed82a23d14a59f29e365
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-10 16:09:30 +0900

    change htpassword

--- master_server/install_munin_server/install_munin.sh
+++ master_server/install_munin_server/install_munin.sh
@@ -5,7 +5,7 @@ yum install -y epel-release
 file_path=/userdata/master_server/install_munin_server
 yum install -y nginx httpd-tools
 yum install -y munin munin-node rrdtool
-htpasswd -b -c /etc/nginx/munin-htpasswd.users muninadmin muninadmin123
+htpasswd -b -c /etc/nginx/htpasswd.users muninadmin muninadmin123
 yes | rm -rf /etc/nginx/conf.d/default.conf
 yes | cp -rp $file_path/munin.conf /etc/nginx/conf.d/
 service nginx restart
