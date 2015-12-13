b384855b53a489f767ed19fb1ca560137696edf1
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-13 14:21:20 +0900

    change kibana

--- master_server/install_elk_server/install_kibana.sh
+++ master_server/install_elk_server/install_kibana.sh
@@ -9,8 +9,8 @@ cd /opt;tar xvf kibana-4.3.0-linux-x64.tar.gz
 mv kibana-4.3.0-linux-x64 kibana
 yes | cp -rf $file_path/kibana.yml /opt/kibana/config/
 chown -R kibana:kibana /opt/kibana
-cd /etc/init.d && sudo curl -o kibana https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/fc5025c3fc499ad8262aff34ba7fde8c87ead7c0/kibana-4.x-init
-cd /etc/default && sudo curl -o kibana https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/fc5025c3fc499ad8262aff34ba7fde8c87ead7c0/kibana-4.x-default
+curl -o /etc/init.d/kibana https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/fc5025c3fc499ad8262aff34ba7fde8c87ead7c0/kibana-4.x-init
+curl -o /etc/default/kibana https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/fc5025c3fc499ad8262aff34ba7fde8c87ead7c0/kibana-4.x-default
 chmod 755 /etc/init.d/kibana
 chmod 755 /etc/default/kibana
 service kibana start
--- master_server/install_elk_server/install_nginx.sh
+++ master_server/install_elk_server/install_nginx.sh
@@ -4,6 +4,7 @@ file_path=/userdata/master_server/install_elk_server
 yum install -y nginx httpd-tools
 yes | cp -rp $file_path/nginx.conf /etc/nginx/
 yes | cp -rp $file_path/kibana.conf /etc/nginx/conf.d/
+htpasswd -b -c /etc/nginx/htpasswd.users kbn kbn
 rm -rf /etc/nginx/conf.d/default.conf
 service nginx start
 chkconfig nginx on
