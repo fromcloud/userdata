a1a561542d5e69ebb9b2f1ccdac6f561b45c1bf7
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-13 14:55:27 +0900

    change munin

--- master_server/install_munin_server/install_munin.sh
+++ master_server/install_munin_server/install_munin.sh
@@ -6,6 +6,7 @@ file_path=/userdata/master_server/install_munin_server
 yum install -y nginx httpd-tools
 yum install -y munin munin-node rrdtool
 yes | rm -rf /etc/nginx/conf.d/default.conf
+yes | cp -rp $file_path/nginx.conf /etc/nginx/
 yes | cp -rp $file_path/munin-web.conf /etc/nginx/conf.d/
 yes | cp -rp $file_path/munin.conf /etc/munin/
 htpasswd -b -c /var/www/munin-htpasswd mn mn
