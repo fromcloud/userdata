343c51bb028f40645edce3a1ab6ea9cfc0d47cb8
root <root@hsw-linux001.cs32f7cloud.internal>
2015-12-13 13:11:06 +0900

    change nginx

--- master_server/install_elk_server/install_nginx.sh
+++ master_server/install_elk_server/install_nginx.sh
@@ -4,5 +4,6 @@ file_path=/userdata/master_server/install_elk_server
 yum install -y nginx httpd-tools
 yes | cp -rp $file_path/nginx.conf /etc/nginx/
 yes | cp -rp $file_path/kibana.conf /etc/nginx/conf.d/
+rm -rf /etc/nginx/conf.d/default.conf
 service nginx start
 chkconfig nginx on
